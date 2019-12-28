6d675e7c6ff08aee5e2a116f81ff7bf7cd60e058
kamil <kd.debinski@gmail.com>
2019-12-28 18:21:17 +0100

    aws key from env variable

--- app/src/main/java/pl/pk/zpi/networking/ApiPaths.kt
+++ app/src/main/java/pl/pk/zpi/networking/ApiPaths.kt
@@ -1,7 +1,6 @@
 package pl.pk.zpi.networking
 
 const val BASE_URL = "https://n1v4frzlvh.execute-api.eu-west-1.amazonaws.com/"
-const val API_KEY = ""
 
 const val HEADER_API_KEY = "x-api-key"
 
--- app/src/main/java/pl/pk/zpi/networking/Service.kt
+++ app/src/main/java/pl/pk/zpi/networking/Service.kt
@@ -2,7 +2,7 @@ package pl.pk.zpi.networking
 
 import io.reactivex.Completable
 import io.reactivex.Single
-import okhttp3.MultipartBody
+import pl.pk.zpi.BuildConfig
 import pl.pk.zpi.models.AuthRequest
 import pl.pk.zpi.models.LoginResponse
 import pl.pk.zpi.models.PhotoUploadRequest
@@ -13,24 +13,24 @@ interface Service {
     @POST(REGISTER)
     fun register(
         @Body request: AuthRequest,
-        @Header(HEADER_API_KEY) apiKey: String = API_KEY
+        @Header(HEADER_API_KEY) apiKey: String = BuildConfig.API_KEY
     ): Completable
 
     @POST(LOGIN)
     fun login(
         @Body request: AuthRequest,
-        @Header(HEADER_API_KEY) apiKey: String = API_KEY
+        @Header(HEADER_API_KEY) apiKey: String = BuildConfig.API_KEY
     ): Single<LoginResponse>
 
     @GET(PHOTOS)
     fun getPhotos(
-        @Header(HEADER_API_KEY) apiKey: String = API_KEY
+        @Header(HEADER_API_KEY) apiKey: String = BuildConfig.API_KEY
     ): Single<List<String>>
 
     @POST(PHOTOS)
     fun uploadPhoto(
         @Body request: PhotoUploadRequest,
-        @Header(HEADER_API_KEY) apiKey: String = API_KEY
+        @Header(HEADER_API_KEY) apiKey: String = BuildConfig.API_KEY
     ): Completable
 
 }
